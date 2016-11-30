7af6dbe94e0445f5928befb0c5870b350463da71
josephcode773 <josephcode773@gmail.com>
2016-11-30 14:51:45 -0600

    brainstorming commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 /**
  * Created by lamars on 11/30/16.
+ *
+ * http://fontawesome.io/cheatsheet/
+ *
+ *
  */
+
+<form class="uk-search data-uk-search="{source:'https://en.wikipedia.org/w/api.php?action=opensearch&search=zyz&limit=1&namespace=0&format=json'}"">
