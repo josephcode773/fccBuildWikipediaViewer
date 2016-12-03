165b943540febb6aaa2ea19928fe8ef8875a107a
josephcode773 <josephcode773@gmail.com>
2016-12-02 23:46:34 -0600

    for loop problem resolved, now cleanup

--- script.js
+++ script.js
@@ -2,25 +2,39 @@ $(document).ready(function () {
 
     $('#target').submit(function (event) {
         event.preventDefault();
-        var searchResults = '';
+        //var searchResults = '';
         var searchQuery = "https://en.wikipedia.org/w/api.php?action=opensearch&callback=?&limit=10&namespace=0&format=json&search=";
         var searchTerm = $("#search-box").val();
+        //var searchResults = '';
         $.getJSON(searchQuery + searchTerm, function (data) {
+            var searchResults = "";
             searchResults = data;
-            console.log(data)
-        });
-        var txt1 = function () {
+
             var resultTitle = "";
             var resultHtml = "";
-            for (var x = 0; x < 10; x++) {
-                resultTitle = searchResults[1][x];
-                resultHtml = searchResults[3][x];
-                var resultResult = ('<a href="url">link text</a>');
-                return resultResult;
-            }
-            //document.getElementById("demo").innerHTML = resultResult;
-        };
-        $("ol").append(txt1)
+            var resultResult = "";
+            var txt1 = function () {
+
+                for (var x = 0; x < 10; x++) {
+                    resultTitle = searchResults[1][x];
+                    resultHtml = searchResults[3][x];
+
+                    resultResult = '<li><a href=' + resultHtml + '>' + resultTitle + '</a></li>';
+
+                    //console.log(resultResult);
+                    $("ol").append(resultResult);
+
+                }
+                //document.getElementById("demo").innerHTML = resultResult;
+            };
+            //debugger;
+            $("ol").append(txt1());
+
+            //searchResults = data;
+            //console.log(searchResults)
+            //$.ajaxSetup({ cache: false});
+        });
+
 
 
     })
